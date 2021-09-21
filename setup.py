256bb34b3a7977bc6af65490a4e4c3f272b8bb14
Rod Morison <rmorison@users.noreply.github.com>
2021-09-21 12:07:29 -0700

    Cleanup for pypi release
    - proper pipenv install in README, fix setup.cfg typo
    - realign version info in python source
    - python_requires >=3.

--- setup.py
+++ setup.py
@@ -21,17 +21,14 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Rod Morison",
     author_email='rmorison@aspiration.com',
-    python_requires='>=3.5',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="Django forms icing on the Slack App cake",
     install_requires=requirements,
@@ -45,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aspiration-labs/slack_forms',
-    version='0.1.0',
+    version='0.2.1',
     zip_safe=False,
 )
