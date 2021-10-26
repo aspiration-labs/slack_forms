63328cf11f7589fe494a05c213deee2895848eab
Rod Morison <47120502+rmasp@users.noreply.github.com>
2021-10-25 18:51:26 -0700

    Mock slack bolt App class

--- tests/fixtures/slack.py
+++ tests/fixtures/slack.py
@@ -1,7 +1,23 @@
 import pytest  # type: ignore
-from slack_bolt import App
+from typing import Dict, Callable
+
+
+class MockSlackBoltApp:
+
+    view_handlers: Dict[str, Callable] = {}
+    action_handlers: Dict[str, Callable] = {}
+
+    def view(self, callback_id):
+        def set_handler(handler: Callable):
+            self.view_handlers[callback_id] = handler
+        return set_handler
+
+    def action(self, action_id):
+        def set_handler(handler: Callable):
+            self.action_handlers[action_id] = handler
+        return set_handler
 
 
 @pytest.fixture
 def slack_app():
-    return App()
+    return MockSlackBoltApp()
